4288f43e85e359e346987e8b09459c0d7850561c
Danny Engelman <danny@engelman.nl>
2021-07-10 20:25:21 +0200

    added JS files

--- counter.de3.js
+++ counter.de3.js
@@ -9,12 +9,13 @@ customElements.define(
         "p{font-size:200%;width:4rem;display:inline-block;text-align:center}" +
         "button{font-size:200%;width:4rem;height:4rem;border:none;border-radius:10px;background:#2e8b57;color:#fff}" +
         "</style>" +
-        "<button onclick=this.getRootNode().host.dec()>-</button><p>0</p><button onclick=this.getRootNode().host.inc()>+</button>";
-      this.count = 0;
-      this.inc = () => this.update(++this.count);
-      this.dec = () => this.update(--this.count);
-      this.update = (count) =>
-        (this.shadowRoot.querySelector("p").innerHTML = count);
+        "<button onclick=this.getRootNode().host.count-->-</button><p>0</p><button onclick=this.getRootNode().host.count++>+</button>";
+    }
+    set count(v){
+      this.shadowRoot.querySelector("p").innerHTML=v;
+    }
+    get count(){
+      return ~~this.shadowRoot.querySelector("p").innerHTML;
     }
   }
 );

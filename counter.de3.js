9e79af13e6f3b27ab8022f6b67aabf98f61046b9
Danny Engelman <danny@engelman.nl>
2021-07-11 11:47:12 +0200

    span

--- counter.de3.js
+++ counter.de3.js
@@ -6,18 +6,18 @@ customElements.define(
         mode: "open",
       }).innerHTML =
         "<style>" +
-        "p{font-size:200%;width:4rem;display:inline-block;text-align:center}" +
+        "span{font-size:200%;width:4rem;display:inline-block;text-align:center}" +
         "button{font-size:200%;width:4rem;height:4rem;border:none;border-radius:10px;background:seagreen;color:white}" +
         "</style>" +
         "<button onclick=this.getRootNode().host.count-- >-</button>" +
-        "<p>0</p>" +
+        "<span>0</span>" +
         "<button onclick=this.getRootNode().host.count++>+</button>";
     }
     set count(v) {
-      this.shadowRoot.querySelector("p").innerHTML = v;
+      this.shadowRoot.querySelector("span").innerHTML = v;
     }
     get count() {
-      return ~~this.shadowRoot.querySelector("p").innerHTML;
+      return ~~this.shadowRoot.querySelector("span").innerHTML;
     }
   }
 );

39c07f85435243185b6e21b5a9510ec037fd8530
Danny Engelman <danny@engelman.nl>
2021-07-11 11:19:35 +0200

    add extra spaces

--- counter.de3.js
+++ counter.de3.js
@@ -5,11 +5,11 @@ customElements.define(
       super().attachShadow({
         mode: "open",
       }).innerHTML =
-        "<style>" +
+        "<style >" +
         "p{font-size:200%;width:4rem;display:inline-block;text-align:center}" +
         "button{font-size:200%;width:4rem;height:4rem;border:none;border-radius:10px;background:seagreen;color:white}" +
-        "</style>" +
-        "<button onclick=this.getRootNode().host.count-- >-</button><p>0</p><button onclick=this.getRootNode().host.count++>+</button>";
+        "</style >" +
+        "<button onclick=this.getRootNode().host.count-- >-</button ><p>0</p ><button onclick=this.getRootNode().host.count++ >+</button >";
     }
     set count(v){
       this.shadowRoot.querySelector("p").innerHTML=v;
